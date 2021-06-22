fd2090317db234c53e7e55753736e8ccd1cdc73c
Your Name <praviprasannan98@gmail.com>
2021-06-22 22:35:06 +0530

    clcultr

---
+++ script.js
@@ -0,0 +1,24 @@
+function displyavalue(num){
+    let inputbox=document.getElementById("rslt")
+    inputbox.value+=num
+}
+//displyvalue=(num)=>rslt.value+=num
+function evaluateExpresion(){
+    let cur_value=rslt.value;
+    let result=eval(cur_value)
+    rslt.value=result;
+
+}
+
+//evaluateExpression()=>reslt.value=eval(rslt.value)
+
+function clearBox(){
+    rslt.value="";
+}
+
+//clearBox()=>rslt.value;
+
+//function removeElement(){
+//    rslt.value=rslt.slice(0,-1)
+//}
+// >
